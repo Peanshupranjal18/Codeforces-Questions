2661fa6a32b18c043f6c91dbec35b614c0871f54
Pranshu Pranjal <pprnch@gmail.com>
2024-04-20 23:33:30 +0530

    Problems

--- 900/Social_Distance.cpp
+++ 900/Social_Distance.cpp
@@ -68,9 +68,16 @@ using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node
 
 void solve()
 {
+    long long n, m;
     cin >> n >> m;
-    v.rs(n);
-    f(i, n) cin >> v[i];
+    long long sum = 0, min_val = 1e9, max_val = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        long long x;
+        cin >> x;
+        sum += x, min_val = min(min_val, x), max_val = max(max_val, x);
+    }
+    cout << (n + sum - min_val + max_val <= m ? "YES" : "NO") << "\n";
 }
 
 int32_t main()
